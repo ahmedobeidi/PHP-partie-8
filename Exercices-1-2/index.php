1147b34f29ef7570ad0c93e77a726e41f5e567c1
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-06 09:21:29 +0100

    DONE: Exercices 1 2

---
+++ Exercices-1-2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercices 1 2</title>
+</head>
+<body>
+    <h2>
+        <?php echo $_SERVER['HTTP_USER_AGENT']; ?>
+    </h2>
+
+    <h2>
+        <?php echo $_SERVER['REMOTE_ADDR']; ?>
+    </h2>
+
+    <h2>
+        <?php echo $_SERVER['HTTP_HOST']; ?>
+    </h2>
+</body>
+</html>
